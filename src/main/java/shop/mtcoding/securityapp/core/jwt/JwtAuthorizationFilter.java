4264ae451c4108a420e7e3d8bdd9a46cae8b527f
clean17 <piw940317@gmail.com>
2023-04-11 16:42:39 +0900

    수정

--- src/main/java/shop/mtcoding/securityapp/core/jwt/JwtAuthorizationFilter.java
+++ src/main/java/shop/mtcoding/securityapp/core/jwt/JwtAuthorizationFilter.java
@@ -23,45 +23,46 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
+// 모든 주소에서 발동
 @Slf4j
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
     public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
-    
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String prefixJwt = request.getHeader(MyJwtProvider.HEADER);
-        if(prefixJwt != null){
-            String jwt = prefixJwt.replace(MyJwtProvider.TOKEN_PREFIX, "");
-            try {
-                DecodedJWT decodedJWT = MyJwtProvider.verify(jwt);
-                Long id = decodedJWT.getClaim("id").asLong();
-                String role = decodedJWT.getClaim("role").asString();
 
-                User user = User.builder().id(id).role(role).build();
-                MyUserDetails myUserDetails = new MyUserDetails(user);
-                Authentication authentication =
-                        new UsernamePasswordAuthenticationToken(
-                                myUserDetails,
-                                myUserDetails.getPassword(),
-                                myUserDetails.getAuthorities()
-                        );
-
-                // 토큰이 있으면 홀더에 임시 세션을 넣는다.
-                // 컨트롤러로 요청은 들어가지만 세션의 권한을 얻을 수는 없다.
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }catch (SignatureVerificationException sve){
-                log.error("토큰 검증 실패");
-                chain.doFilter(request, response);
-                return;
-            }catch (TokenExpiredException tee){
-                log.error("토큰 기한 만료");
-                chain.doFilter(request, response);
-                return;
-            }
+        if (prefixJwt == null) {
+            chain.doFilter(request, response);
+            return;
         }
-        chain.doFilter(request, response);
+
+        String jwt = prefixJwt.replace(MyJwtProvider.TOKEN_PREFIX, "");
+        try {
+            DecodedJWT decodedJWT = MyJwtProvider.verify(jwt);
+            Long id = decodedJWT.getClaim("id").asLong();
+            String role = decodedJWT.getClaim("role").asString();
+
+            User user = User.builder().id(id).role(role).build();
+            MyUserDetails myUserDetails = new MyUserDetails(user);
+            Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            myUserDetails,
+                            myUserDetails.getPassword(),
+                            myUserDetails.getAuthorities()
+                    );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (SignatureVerificationException sve) {
+            log.error("토큰 검증 실패");
+        } catch (TokenExpiredException tee) {
+            log.error("토큰 만료됨");
+        } finally {
+            chain.doFilter(request, response);
+        }
     }
 }
+// 토큰이 있으면 홀더에 임시 세션을 넣는다.
+// 컨트롤러로 요청은 들어가지만 세션의 권한을 얻을 수는 없다.
